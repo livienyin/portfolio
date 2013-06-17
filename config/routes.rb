11eaad35e4f048f2cb19aa73ffe0ee60202045b8
Livien Yin <LivienYin@gmail.com>
2013-06-16 20:17:48 -0700

    Add images and fix styling on home pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 LivienyinCom::Application.routes.draw do
-  
-
-  
-
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-
-  devise_for :users
-
-  
-
-  get '/' => 'pages#home'
+  root :to => 'pages#home'
   get '/page/:id' => 'pages#get_page'
   get '/page_info' => 'pages#get_ordered_page_info'
 end
